a6c53c595c2e523f89e5b7a44c810f05e49f0b57
Matt Nowakowski <nowakowskim98@gmail.com>
2022-07-02 14:38:41 -0400

    Implemented command scenarios

--- src/project/ExercisePlanner/main.cpp
+++ src/project/ExercisePlanner/main.cpp
@@ -12,23 +12,30 @@ using namespace MuscleUse;
 
 int main(int, char**) {
     auto muscles = LoadData::LoadMuscles("muscles.json");
-
     auto exercises = LoadData::LoadExercises("exercises.json");
 
     string command;
-    cin >> command;
+    bool exit = false;
 
-    if(command == "unused") {
-        cout << "Unused muscles: " << endl;
-        auto unusedMuscles = GetUnusedMuscles(exercises, muscles);
-        for(auto it = unusedMuscles.begin(); it != unusedMuscles.end(); ++it)
-            cout << it->name << endl;
-    } else if (command == "used") {
-        cout << "\nUsed muscles: " << endl;
-        auto usedMuscles = GetUsedMuscles(exercises, muscles);
-        for(auto it = usedMuscles.begin(); it != usedMuscles.end(); ++it)
-            cout << it->name << endl;
-    } else cout << "Unknown" << endl;
+    do {
+        cout << "Command: ";
+        cin >> command;
 
+        if(command == "unused") {
+            cout << "Unused muscles: " << endl;
+            auto unusedMuscles = GetUnusedMuscles(exercises, muscles);
+            for(auto it = unusedMuscles.begin(); it != unusedMuscles.end(); ++it)
+                cout << it->name << endl;
+        } else if (command == "used") {
+            cout << "\nUsed muscles: " << endl;
+            auto usedMuscles = GetUsedMuscles(exercises, muscles);
+            for(auto it = usedMuscles.begin(); it != usedMuscles.end(); ++it)
+                cout << it->name << endl;
+        }
+        else if (command == "exit") exit = true;
+        else cout << "Unknown" << endl;
+
+    } while (!exit);
+    
     return EXIT_SUCCESS;
 }
